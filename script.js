acef79f1df1b10b3281db5f2d6be743431e22e0b
Nikita Shabulin <nwpress@yandex.ru>
2020-07-05 20:26:18 +0700

    доделана вторая часть 18 урока

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+    const greetings = document.querySelector('#greetings'),
+        today = document.querySelector('#today'),
+        currentTime = document.querySelector('#current_time'),
+        newYear = document.querySelector('#new_year'),
+        currentDate = new Date(),
+        newYearDate = new Date("31 Dec 2020 23:59:59");
+
+    const dayOfTheWeek = [
+        'Воскресенье',
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота'
+    ];
+
+    const timesOfDay = time => {
+        if (time >= 5 && time <= 11) {
+            return "Доброе утро";
+        } else if (time >= 12 && time <= 16) {
+            return "Добрый день";
+        } else if (time >= 17 && time <= 23) {
+            return "Добрый вечер";
+        } else if (time >= 24 && time <= 4) {
+            return "Доброй ночи";
+        }
+    };
+
+    const convertHourseAMPM = time => {
+        let amPm = 'PM',
+            newHours = (time + 24) % 12;
+
+        if (time < 12) {
+            amPm = 'AM';
+        }
+
+        return {amPm, newHours};
+    };
+
+    const newHours = convertHourseAMPM(currentDate.getHours());
+    const daysUntilNewYear = Math.floor((newYearDate - currentDate) / 1000 / 60 / 60 / 24);
+
+    greetings.textContent = timesOfDay(currentDate.getHours());
+    today.textContent = `Сегодня: ${dayOfTheWeek[currentDate.getDay()]}`;
+    currentTime.textContent = `Текущее время: ${newHours.newHours}:${currentDate.getMinutes()}:` +
+        `${currentDate.getSeconds()} ${newHours.amPm}`;
+    newYear.textContent = `До нового года осталось ${daysUntilNewYear} дней`;
+    
+});
