632bfce2b44f335f4aaf05e53259c1dc9a279073
Nikita Shabulin <nwpress@yandex.ru>
2020-07-10 23:02:20 +0700

    Дописан калькулятор

--- js/script.js
+++ js/script.js
@@ -312,4 +312,49 @@ window.addEventListener("DOMContentLoaded", () => {
     calcContainer.addEventListener("input", event => {
         inputDigitValid(event.target);
     });
+
+    //Calculator
+
+    const calc = (price = 100) => {
+        const calcType = document.querySelector(".calc-type"),
+            calcSquare = document.querySelector(".calc-square"),
+            calcDay = document.querySelector(".calc-day"),
+            calcCount = document.querySelector(".calc-count"),
+            totalValue = document.getElementById("total");
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+            let squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * squareValue * typeValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        };
+
+        calcContainer.addEventListener("change", event => {
+            const target = event.target;
+
+            if (target.matches(".calc-type") || target.matches(".calc-square") ||
+            target.matches(".calc-day") || target.matches(".calc-count")) {
+                countSum();
+            }
+        });
+    };
+
+    calc(100);
 });
