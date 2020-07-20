cd22100439b43ebfb2aa57576babd321fb629a83
Nikita Shabulin <nwpress@yandex.ru>
2020-07-20 20:06:39 +0700

    26 услж задание

--- server.php
+++ server.php
@@ -1,2 +1,3 @@
 <?php
+sleep(3);
 echo $_SERVER['REMOTE_ADDR'];
--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -2,7 +2,6 @@
 
 const sendForm = () => {
   const errorMsg = "Что-то пошло не так...",
-    loadMsg = "Загрузка...",
     successMsg = "Спасибо! Мы скоро свяжемся с вами.",
     body = document.querySelector("body"),
     statusMsg = document.createElement("div");
@@ -40,12 +39,22 @@ const sendForm = () => {
   body.addEventListener("submit", event => {
     event.preventDefault();
     const ourForm = event.target;
-
+    statusMsg.textContent = "";
     if (ourForm.tagName === "FORM") {
       const formData = new FormData(ourForm);
 
       ourForm.appendChild(statusMsg);
-      statusMsg.textContent = loadMsg;
+      statusMsg.insertAdjacentHTML("beforeend", `<div class='sk-cube-grid'>
+      <div class='sk-cube sk-cube-1'></div>
+      <div class='sk-cube sk-cube-2'></div>
+      <div class='sk-cube sk-cube-3'></div>
+      <div class='sk-cube sk-cube-4'></div>
+      <div class='sk-cube sk-cube-5'></div>
+      <div class='sk-cube sk-cube-6'></div>
+      <div class='sk-cube sk-cube-7'></div>
+      <div class='sk-cube sk-cube-8'></div>
+      <div class='sk-cube sk-cube-9'></div>
+      </div>`);
       
       postData(formData)
         .then((response) => {
